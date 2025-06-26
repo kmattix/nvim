f7c90f3da07203167acc3b61165ab84385fe1a02
Kevin Mattix <kevinmattix@proton.me>
2025-06-25 19:54:45 -0600

    new globals

--- init.lua
+++ init.lua
@@ -2,9 +2,16 @@ require("config.lazy")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+
 vim.opt.cursorline = true
+vim.opt.scrolloff = 5
 
 vim.wo.number = true
 vim.wo.relativenumber = true
