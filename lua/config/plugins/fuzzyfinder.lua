5e5bdae743b35fd016653eefc9a4e0b56bfec850
Kevin Mattix <kevinmattix@proton.me>
2025-06-25 21:27:30 -0600

    fixed fuzzy find for top level git repos

--- lua/config/plugins/fuzzyfinder.lua
+++ lua/config/plugins/fuzzyfinder.lua
@@ -1,46 +1,50 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	-- or                              , branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		-- Keymaps
-		vim.keymap.set('n', '<C-p>', function()
-			local buf_dir = vim.fn.expand('%:p:h')
-			local ok = os.execute('git -C ' ..
-				vim.fn.shellescape(buf_dir) .. ' rev-parse --is-inside-work-tree >/dev/null 2>&1') == 0
-			if ok then
-				local root = vim.fn.systemlist('git -C ' .. vim.fn.shellescape(buf_dir) .. ' rev-parse --show-toplevel')[1]
-				require('telescope.builtin').git_files({ cwd = root })
-			else
-				require('telescope.builtin').find_files({ cwd = buf_dir })
-			end
-		end)
-		vim.keymap.set('n', '<leader>ps', function()
-			require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") })
-		end)
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  -- or                              , branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    -- Keymaps
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<C-p>', function()
+      local function get_git_root(dir)
+        local dot = vim.fn.finddir(".git", dir .. ";")
+        if dot == "" then return nil end
+        return vim.fn.fnamemodify(dot, ":h")
+      end
+      local buf_dir = vim.fn.expand('%:p:h')
+      local root = get_git_root(buf_dir)
+      if root then
+        builtin.git_files({ cwd = root })
+      else
+        builtin.find_files({ cwd = buf_dir })
+      end
+    end)
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
 
-		local telescope = require('telescope')
-		local telescopeConfig = require('telescope.config')
+    local telescope = require('telescope')
+    local telescopeConfig = require('telescope.config')
 
-		-- Clone the default Telescope configuration
-		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+    -- Clone the default Telescope configuration
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
-		-- Add flags to follow symlinks and include hidden files
-		table.insert(vimgrep_arguments, '-L')       -- Follow symlinks
-		table.insert(vimgrep_arguments, '--hidden') -- Include hidden files
-		table.insert(vimgrep_arguments, '--glob')
-		table.insert(vimgrep_arguments, '!**/.git/*') -- Exclude .git directories
+    -- Add flags to follow symlinks and include hidden files
+    table.insert(vimgrep_arguments, '-L')         -- Follow symlinks
+    table.insert(vimgrep_arguments, '--hidden')   -- Include hidden files
+    table.insert(vimgrep_arguments, '--glob')
+    table.insert(vimgrep_arguments, '!**/.git/*') -- Exclude .git directories
 
-		telescope.setup({
-			defaults = {
-				vimgrep_arguments = vimgrep_arguments,
-			},
-			pickers = {
-				find_files = {
-					find_command = { 'rg', '--files', '--glob', '!**/.git/*', '-L' },
-				},
-			},
-		})
-	end,
+    telescope.setup({
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+      },
+      pickers = {
+        find_files = {
+          find_command = { 'rg', '--files', '--glob', '!**/.git/*', '-L' },
+        },
+      },
+    })
+  end,
 }
